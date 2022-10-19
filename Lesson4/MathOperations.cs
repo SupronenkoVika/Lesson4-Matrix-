88db23f8d36b55952970474390e864b50727519b
Viktoryia Supronenko <vika.supronenko@icloud.com>
2022-10-20 00:13:12 +0300

    Add project files.

---
+++ Lesson4/MathOperations.cs
@@ -0,0 +1,76 @@
+﻿namespace Lesson4
+{
+    public class MathOperations
+    {
+        public void AmountOfPosAndNegNumbers(int[,] arr, int str, int column)
+        {
+            int positive = 0;
+            int negative = 0;
+            for (int i = 0; i < str; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (arr[i, j] > 0)
+                    {
+                        positive++;
+                    }
+                    else
+                    {
+                        negative++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Amount of posive numbers: " + positive);
+            Console.WriteLine("Amount of negative numbers: " + negative);
+        }
+
+        public void ArrSort(int[,] arr, int str, int column)
+        {
+            for (int k = 0; k < str; k++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    for (int i = 0; i < column - 1; i++)
+                    {
+                        if (arr[k, i] > arr[k, i + 1])
+                        {
+                            int t = arr[k, i];
+                            arr[k, i] = arr[k, i + 1];
+                            arr[k, i + 1] = t;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < str; i++)
+            {
+                for (int j = 0; j < column; j++)
+                    Console.Write(String.Format("{0,5}", arr[i, j]));
+                Console.WriteLine();
+            }
+        }
+
+        public void Invers(int[,] arr, int str, int column)
+        {
+            for (int i = 0; i < arr.GetLength(1) / 2; i++)
+            {
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    int t = arr[j, i];
+                    arr[j, i] = arr[j, arr.GetLength(1) - i - 1];
+                    arr[j, arr.GetLength(1) - i - 1] = t;
+                }
+            }
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                    Console.Write(String.Format("{0,5}", arr[i, j]));
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Lesson4/Program.cs
@@ -0,0 +1,68 @@
+﻿using Lesson4;
+
+int x, y;
+
+Console.WriteLine("Enter the array dimension: ");
+
+Console.Write("X = ");
+x = int.Parse(Console.ReadLine());
+
+Console.Write("Y = ");
+y = int.Parse(Console.ReadLine());
+
+
+int[,] array = new int[x, y];
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.WriteLine("x - " + i + " y - " + j);
+        array[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Array output: ");
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        Console.Write(String.Format("{0,5}", array[i, j]));
+    }
+    Console.WriteLine();
+}
+
+MathOperations mathOp = new MathOperations();
+
+Console.WriteLine("Choose the action.");
+Console.WriteLine("'p' - amount of positive and negative numbers");
+Console.WriteLine("'s' - array sorting");
+Console.WriteLine("'i' - array inversion");
+char act = Convert.ToChar(Console.ReadLine());
+
+switch (act)
+{
+    case 'p':
+        mathOp.AmountOfPosAndNegNumbers(array, x, y);
+        break;
+    case 's':
+        mathOp.ArrSort(array, x, y);
+        break;
+    case 'i':
+        mathOp.Invers(array, x, y);
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
